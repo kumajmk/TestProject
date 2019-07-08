a2977607af0ff3e3a6651f9eaff40a8a201fea2e
J N Maruthi Kumari <mkumarijn1979@gmail.com>
2019-07-08 13:28:36 +0530

    Test Git

---
+++ TestGit/src/main/java/test/java/TestGit/App.java
@@ -0,0 +1,13 @@
+package test.java.TestGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
